b964dfecca482e7ff76725216e2d0796ea1b06f4
Kasyad3 <paulo.hprosa@senacsp.edu.br>
2024-08-20 21:51:37 -0300

    feat: Calculando a média das notas do aluno

--- script.js
+++ script.js
@@ -1,3 +1,24 @@
+//Exibir no terminal
 console.log("Olá Mundo");
+//Exibir no navegador
+document.write("Olá Navegador! ");
+//Um alerta
+alert("Alerta de confirmação");
 
-document.write("Olá Navegador! ")
+//string
+
+//Captura o nome do Aluno
+var nome = window.prompt("Informe o Nome:");
+//Guardar a primeira nota
+var nota1 = window.prompt("Digite a nota 1;");
+//Converte a nota de texto para numero decimal 
+nota1 = parseFloat(nota1);
+//Guarda a segunda nota
+var nota2 = window.prompt("Digite a nota 2:");
+//Converte a nota de texto para numero decimal
+nota2 = parseFloat(nota2);
+//calculo a média das duas notas
+var media = (nota1 + nota2)/2;
+//Gero a mensagem de exibição  do nome e da média do aluno
+var mensagem = nome + ", Você teve a média: " + media;
+window.alert(mensagem);
